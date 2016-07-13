5cfbf010d78a8e33f66032d4e86670efbd7a345c
Adhyan <adhyan_srivastava95@yahoo.com>
2016-07-13 20:18:19 +0530

    Fetched Notifications in a Notification tray

--- app/scripts/controllers/notification/NotificationsController.js
+++ app/scripts/controllers/notification/NotificationsController.js
@@ -1,8 +1,14 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
         NotificationsController: function (scope, rootScope, resourceFactory, location, notificationResponseHeaderFactory) {
+
+            var objTypeUrlMap = {
+                'client' : '/viewclient/'
+            };
+
             scope.notifications = [];
             scope.notificationsPerPage = 15;
+            scope.isNotificationTrayOpen = false;
             scope.getResultsPage = function (pageNumber) {
                 var items = resourceFactory.notificationsResource.getAllNotifications({
                     offset : ((pageNumber-1) * scope.notificationsPerPage),
@@ -14,21 +20,38 @@
             scope.initPage = function () {
                 var items = resourceFactory.notificationsResource.getAllNotifications({
                     offset: 0,
-                    limit: scope.notificationsPerPage
+                    limit: scope.notificationsPerPage || 10
                 }, function (data) {
                     scope.totalNotifications = data.totalFilteredRecords;
                     scope.notifications = data.pageItems;
                 });
             };
             scope.initPage();
-            console.log(notificationResponseHeaderFactory.response);
+            scope.navigateToAction = function(notification){
+                if(!notification.objectType || typeof(notification.objectType) !=='string'){
+                    console.error('no object type found');
+                    return;
+                }
+                notification.objectType = notification.objectType.toLowerCase();
+                if(!objTypeUrlMap[notification.objectType] ){
+                    console.error('objectType not found in map. Invalid object type');
+                    return;
+
+                }
+                location.path(objTypeUrlMap[notification.objectType.toLowerCase()]+notification.objectId);
+            };
+            scope.fetchItemsInTray = function () {
+                if (scope.isNotificationTrayOpen) {
+                    scope.initPage();
+                }
+            };
         }
     });
     mifosX.ng.application.controller('NotificationsController', ['$scope', '$rootScope', 'ResourceFactory', '$location',
         'NotificationResponseHeaderFactory' ,mifosX.controllers.NotificationsController]).run(function ($log, $rootScope) {
         $log.info("NotificationsController initialized");
         $rootScope.$on('eventFired', function(event, data) {
-            console.log(data);
+
         });
     });
 }(mifosX.controllers || {}));
