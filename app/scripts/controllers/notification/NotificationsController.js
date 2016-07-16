f4e3799a2f3078328b6125ccf337696f7af68d0a
Adhyan <adhyan_srivastava95@yahoo.com>
2016-07-16 20:41:21 +0530

    Added functionality to merge fetched notifications with notifications stored in local storage

--- app/scripts/controllers/notification/NotificationsController.js
+++ app/scripts/controllers/notification/NotificationsController.js
@@ -1,9 +1,11 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        NotificationsController: function (scope, rootScope, resourceFactory, location, notificationResponseHeaderFactory) {
+        NotificationsController: function (scope, rootScope, resourceFactory, location,
+                                           notificationResponseHeaderFactory, localStorageService) {
 
             var objTypeUrlMap = {
-                'client' : '/viewclient/'
+                'client' : '/viewclient/',
+                'group' : '/viewgroup/'
             };
 
             scope.notifications = [];
@@ -16,6 +18,45 @@
                 }, function (data) {
                     scope.notifications = data.pageItems;
                 });
+            };
+            scope.initNotificationTray = function() {
+              var items = resourceFactory.notificationsResource.getAllUnreadNotifications({
+                  offset: 0,
+                  limit: scope.notificationsPerPage || 10
+              }, function(data){
+                  var readNotifications = localStorageService.getFromLocalStorage("notifications");
+                  if (data.pageItems.length == 0) {
+                      console.log("If was called");
+                      if (readNotifications == null) {
+                          scope.initPage();
+                          console.log("There are no local storage notifications available.")
+                      } else {
+                          scope.notifications = readNotifications;
+                          console.log("Fetching from local storage")
+                      }
+                  } else {
+                      console.log("Else was called");
+                      console.log(readNotifications);
+                      if (readNotifications == null) {
+                          scope.notifications = data.pageItems;
+                          console.log("There are no local storage notifications available.")
+                      } else {
+                          scope.notifications = data.pageItems.concat
+                          (readNotifications
+                                  .slice(0, Math.abs(readNotifications.length - data.pageItems.length + 1)));
+                          console.log(scope.notifications);
+                          console.log("There are local storage notifications. Merging it with the new ones :)")
+                      }
+                      resourceFactory.notificationsResource.update();
+                  }
+                  localStorageService.removeFromLocalStorage("notifications");
+                  localStorageService.addToLocalStorage("notifications", JSON.stringify(scope.notifications));
+              })
+            };
+            scope.fetchItemsInTray = function () {
+                if (scope.isNotificationTrayOpen) {
+                    scope.initNotificationTray();
+                }
             };
             scope.initPage = function () {
                 var items = resourceFactory.notificationsResource.getAllNotifications({
@@ -40,15 +81,11 @@
                 }
                 location.path(objTypeUrlMap[notification.objectType.toLowerCase()]+notification.objectId);
             };
-            scope.fetchItemsInTray = function () {
-                if (scope.isNotificationTrayOpen) {
-                    scope.initPage();
-                }
-            };
         }
     });
     mifosX.ng.application.controller('NotificationsController', ['$scope', '$rootScope', 'ResourceFactory', '$location',
-        'NotificationResponseHeaderFactory' ,mifosX.controllers.NotificationsController]).run(function ($log, $rootScope) {
+        'NotificationResponseHeaderFactory' , 'localStorageService', mifosX.controllers.NotificationsController])
+        .run(function ($log, $rootScope) {
         $log.info("NotificationsController initialized");
         $rootScope.$on('eventFired', function(event, data) {
 
